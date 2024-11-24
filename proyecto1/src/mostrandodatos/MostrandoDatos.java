0e5aca479da37cb5ff225041c74e4ffe66b1fc7c
Carlos Gonzalez <24.damt.carlosalejandrogonzalez@fundacionmontessori.com>
2024-11-24 15:14:12 +0100

    proyectoEclipse

---
+++ proyecto1/src/mostrandodatos/MostrandoDatos.java
@@ -0,0 +1,8 @@
+package mostrandodatos;
+
+public class MostrandoDatos {
+	public static void main(String[] args) {
+		System.out.println("Nombre: Carlos Alejandro");
+		System.out.println("Apellido:  Gonzalez Andrades");
+	}
+}
